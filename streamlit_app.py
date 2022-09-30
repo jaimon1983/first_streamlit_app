0b0b4562eb9bb33f34b03fb89bd861ec2866b36d
jaimon1983 <47558797+jaimon1983@users.noreply.github.com>
2022-09-30 14:04:45 +0530

    Update streamlit_app.py
    Added Header and Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('The New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Breakfst Item 1')
+streamlit.text('Breakfst Item 2')
+streamlit.text('Breakfst Item 3')
