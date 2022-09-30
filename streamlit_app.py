980a48138dfbe2fc32d0ffeffd04a0f9c2b08c00
jaimon1983 <47558797+jaimon1983@users.noreply.github.com>
2022-09-30 14:06:15 +0530

    Update streamlit_app.py
    Text Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('The New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Breakfst Item 1')
-streamlit.text('Breakfst Item 2')
-streamlit.text('Breakfst Item 3')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
